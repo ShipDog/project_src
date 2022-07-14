3f1b16bd52ac8bd2b631ab65e3b32e03affc4ca2
bohamian <87678241+bohamian@users.noreply.github.com>
2022-07-14 12:20:03 +0900

    first commit

--- templates/userdata.sh
+++ templates/userdata.sh
@@ -6,15 +6,15 @@ sudo yum install -y docker git
 # docker
 sudo usermod -aG docker ec2-user
 systemctl enable docker
-systemctl start docker
+systemctl start docker:
 
 # docker-compose
-sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-Linux-x86_64" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 
 # source
 cd /home/ec2-user
 git clone https://github.com/bohamian/project_src.git
-chown -R ec2-user:ec2-user devops_06_03_jenkins
+sudo chown -R ec2-user:ec2-user project_src
 cd /home/ec2-user/project_src/jenkins_remote_docker
 make run
